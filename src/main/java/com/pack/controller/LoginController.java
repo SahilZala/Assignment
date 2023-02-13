af7bce80a63f365be24a37334e2e665aaf22a85e
sahilz <zala4011@gmail.com>
2023-02-13 09:47:37 +0530

    dumy controller implementation

--- src/main/java/com/pack/controller/LoginController.java
+++ src/main/java/com/pack/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,5 +55,10 @@ public class LoginController {
 		}
 	}
 	
+	
+	@GetMapping("/dumy")
+	public String dumy() {
+		return "sahil dumy";
+	}
 	
 }
